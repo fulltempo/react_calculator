a8e962d1c31c40e399ecd983344652796c93583b
fulltempo <sa10652970@foxmail.com>
2023-10-23 19:38:28 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// let output = document.getElementById("output");
 
 function App() {
   const [output, setOutput] = React.useState('0');
@@ -12,50 +11,31 @@ function App() {
       // output = value;
       setOutput(value);
     }
-    /*else if(output.innerHTML === "√"){
-      output.innerHTML = ;  
-    }*/
     else {
-      // output.innerHTML += value;
       setOutput(output + value);
     }
   }
 
   function clearOutput() {
-    // output.innerHTML = "0";
     setOutput("0");
   }
 
   function BackSpace() {
-    // if (output.innerHTML.length > 1) {
-    //   output.innerHTML = output.innerHTML.substr(0, output.innerHTML.length - 1);
-    // }
-    // else {
-    //   output.innerHTML = 0;
-    // }
     if (output.length > 1) {
       setOutput(output.substr(0, output.length - 1));
     }
     else {
-      // setOutput(0);
       setOutput('0');
     }
   }
 
   function calculate() {
-    // if (output.innerHTML.includes("√")) {
-    //   var num = output.innerHTML.substring(1, output.innerHTML.length);
-    //   output.innerHTML = eval(Math.sqrt(num));
-    // }
-    // else {
-    //   output.innerHTML = eval(output.innerHTML);
-    // }
     if (output.includes("√")) {
       var num = output.substring(1, output.length);
       setOutput(eval(Math.sqrt(num)));
     }
     else if (output.includes("%")) {
-      setOutput(parseInt(output)/100);
+      setOutput(parseInt(output) / 100);
     }
     else {
       console.log(output);
@@ -64,22 +44,6 @@ function App() {
   }
 
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div className="container">
       <div className="output" id="output">{output}</div>
       <button className="buttonOfAD" onClick={() => clearOutput()} id="clear">AC</button>
